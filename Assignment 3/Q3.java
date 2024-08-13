f2d27a58e0bb65e30900837ace23622086a80d7a
Dishang H. Rana <dishangkumarrana@gmail.com>
2024-08-13 23:23:28 +0530

    Update Q3.java

--- Assignment 3/Q3.java
+++ Assignment 3/Q3.java
@@ -1,6 +1,6 @@
 // Java Program to implement the Living Organisms hierarchy or to implement the Nomenclature system of Chemical Compounds
 public class Q3 {
     public static void main(String[] args) {
-//Create a living organism classes
+
     }
 }
