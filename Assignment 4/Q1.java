526f81ec5c0373436b0586386dc6243ae77c3ab1
thisisdishang <dishangkumarrana@gmail.com>
2024-09-12 20:31:34 +0530

    Update folder for package use

--- Assignment 4/Q1.java
+++
@@ -1,7 +0,0 @@
-package Assignment 4;
-
-public class Q1 {
-    public static void main(String[] args) {
-        // To Do Assignment 4
-    }
-}
---
+++ Assignment4/Q1.java
@@ -0,0 +1,10 @@
+/* Java Program (Use Package) to inherit time class. (i.e. First class has the hours component, Second class has the
+minutes component & the Third class has a seconds component.) Given 2 objects, find the time lapsed between them. */
+
+package Assignment4;
+
+public class Q1 {
+    public static void main(String[] args) {
+        // To Do Assignment 4
+    }
+}
