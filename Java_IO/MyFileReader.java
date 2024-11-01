a7902b2ac7aa2e01f45cc17160159a606e15d642
thisisdishang <dishangkumarrana@gmail.com>
2024-11-01 19:02:14 +0530

    Update MyFileReader.java and MyFileWriter.java

--- Java_IO/MyFileReader.java
+++ Java_IO/MyFileReader.java
@@ -1,9 +1,23 @@
 package Java_IO;
 
-import java.io.;;
+import java.io.*;
 
 public class MyFileReader {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException{
+        File inFile=new File("C:\\Users\\disha\\Downloads\\JavaDeepDive\\Java_IO\\myfile.txt");
+        FileReader fr=null;
+        try{
+            fr=new FileReader(inFile);
+            int ch;
+            while ((ch=fr.read())!=-1) {
+                System.out.print((char)ch);
+            }
+        }
+        catch(IOException e){
+            System.out.println(e);
+        }
+        finally{
+            fr.close();
+        }
     }
 }
--- Java_IO/MyFileWriter.java
+++ Java_IO/MyFileWriter.java
@@ -3,7 +3,18 @@ package Java_IO;
 import java.io.*;
 
 public class MyFileWriter {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        File outFile=new File("C:\\Users\\disha\\Downloads\\JavaDeepDive\\Java_IO\\myfile.txt");
+        FileWriter fout=null;
+        try {
+            fout=new FileWriter(outFile);
+            String s="Beyond the belief";
+            fout.write(s);
+            System.out.println("Success...");
+        } catch (IOException e) {
+            System.out.println(e);
+        }finally{
+            fout.close();
+        }
     }
 }
