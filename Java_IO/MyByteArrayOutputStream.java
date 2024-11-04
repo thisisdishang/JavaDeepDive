35ffe675e8b1a009eac41ffccb773ada96035794
thisisdishang <dishangkumarrana@gmail.com>
2024-11-04 23:10:12 +0530

    Update MyByteArrayOutputStream.java

--- Java_IO/MyByteArrayOutputStream.java
+++ Java_IO/MyByteArrayOutputStream.java
@@ -2,11 +2,18 @@ package Java_IO;
 
 import java.io.*;
 
-/**
- * MyByteArrayOutputStream
- */
 public class MyByteArrayOutputStream {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        FileOutputStream f1=new FileOutputStream("C:\\Users\\disha\\Downloads\\JavaDeepDive\\Java_IO\\f1.txt");
+        FileOutputStream f2=new FileOutputStream("C:\\Users\\disha\\Downloads\\JavaDeepDive\\Java_IO\\f2.txt");
+
+        ByteArrayOutputStream b1=new ByteArrayOutputStream();
         
+        b1.write(65);
+        b1.writeTo(f1);
+        b1.writeTo(f2);
+        System.out.println("Success...");
+        b1.flush();
+        b1.close();
     }
 }
