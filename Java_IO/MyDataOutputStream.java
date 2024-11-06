403c078bee6523511f881c4c9508e531e231643b
thisisdishang <dishangkumarrana@gmail.com>
2024-11-06 21:10:41 +0530

    Implement DataOutputStream

--- Java_IO/MyDataOutputStream.java
+++ Java_IO/MyDataOutputStream.java
@@ -3,7 +3,12 @@ package Java_IO;
 import java.io.*;
 
 public class MyDataOutputStream {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        FileOutputStream file=new FileOutputStream("C:\\Users\\disha\\Downloads\\JavaDeepDive\\Java_IO\\f1.txt");
+        DataOutputStream data=new DataOutputStream(file);
+        data.writeInt(68);
+        data.flush();
+        data.close();
+        System.out.println("Success...");
     }
 }
