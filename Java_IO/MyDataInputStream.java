496494c8d4c61439c01547b839d6acf054cad813
thisisdishang <dishangkumarrana@gmail.com>
2024-11-07 22:35:56 +0530

    Implement DataInputStream

--- Java_IO/MyDataInputStream.java
+++ Java_IO/MyDataInputStream.java
@@ -3,7 +3,15 @@ package Java_IO;
 import java.io.*;
 
 public class MyDataInputStream {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException{
+        FileInputStream input=new FileInputStream("C:\\Users\\disha\\Downloads\\JavaDeepDive\\Java_IO\\myfile.txt");
+        DataInputStream dis=new DataInputStream(input);
+        int count=input.available();
+        byte[] ary=new byte[count];
+        dis.read(ary);
+        for(byte bt:ary){
+            char k=(char)bt;
+            System.out.print(k+"-");
+        }
     }
 }
