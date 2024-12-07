0b348413b1f2d988ffe3acddc997fba0bb83f3eb
thisisdishang <dishangkumarrana@gmail.com>
2024-12-07 18:45:56 +0530

    Update Servlet

--- Servlet/first.java
+++ Servlet/first.java
@@ -3,10 +3,10 @@ package Servlet;
 import java.io.*;
 import javax.servlet.*;
 
-public class First implements Servlet
+public class first implements Servlet
 {
 
-    ServletConfig config=null;  
+    ServletConfig config=null;   
     public void init(ServletConfig config)
     {
         this.config=config;
@@ -18,7 +18,7 @@ public class First implements Servlet
         response.setContentType("text/html");
         PrintWriter out=response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>hello this is simple servlet</h1>");
+        out.println("<h1>Hello, This is First Servlet</h1>");
         out.println("</body></html>");
     }
 
